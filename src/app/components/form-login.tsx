31193f3ac6390040cd8135c46858152167ca9504
v4leng <valem.g_13@hotmail.com>
2023-09-11 16:38:14 -0300

    card-small

--- src/app/components/form-login.tsx
+++ src/app/components/form-login.tsx
@@ -1,30 +1,39 @@
-import ButtonLogin from "../commons/button-login";
+import ButtonLogin from "../commons/button";
 
 export default function FormLogin() {
   return (
-    <div className="text-center w-full  ">
-      <form className="rounded px-8 pt-6 pb-8 mb-4">
-        <div className="mb-8">
+    <div className="text-center w-full items-center">
+      <form className="rounded px-8 pt-6 pb-8 mb-4  ">
+        <div className="mb-8 flex items-center">
           <img src="/login.svg" alt="login" />
         </div>
         <div className="mb-4 ">
           <input
-            className="appearance-none bg-inherit border border-white rounded-lg py-3 px-3 text-white leading-tight focus:outline-none placeholder-white w-full"
+            className="appearance-none bg-inherit border border-white rounded-lg py-3 px-3 text-white leading-tight focus:outline-none placeholder-white h-[45px] w-[300px]"
             id="username"
             type="text"
             placeholder="email@contraseña.com"
           ></input>
         </div>
-        <div className="mb-4">
+        <div className="mb-3 ">
           <input
-            className="appearance-none bg-inherit border border-white rounded-lg py-3 px-3 text-white mb-3 leading-tight focus:outline-none  placeholder-white w-full "
+            className="appearance-none bg-inherit border border-white rounded-lg py-0 px-3 text-white mb-3 leading-tight focus:outline-none  placeholder-white h-[45px] w-[300px]"
             id="password"
             type="password"
             placeholder="**********"
           ></input>
         </div>
-        <div className="mb-8">
-          <ButtonLogin />
+        <div className="mb-4">
+          <ButtonLogin
+            text="Ingresar"
+            className="bg-white	text-stone-600"
+          ></ButtonLogin>
+        </div>
+        <div className="mb-4">
+          <ButtonLogin
+            text="Crear cuenta"
+            className="bg-inherit	text-white border border-white"
+          ></ButtonLogin>
         </div>
         <div className="">
           <a
