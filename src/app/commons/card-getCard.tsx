37f9f99bcbbbfb4bedf92f36c0295fd4f7c0271b
fggutierrez98 <fggutierrez98@gmail.com>
2023-09-13 13:59:34 -0300

    resolve bux

--- src/app/commons/card-getCard.tsx
+++ src/app/commons/card-getCard.tsx
@@ -13,41 +13,41 @@ const GetCard: React.FC = () => {
   useEffect(() => {}, [amount]);
   return (
     <div
-      className="flex items-center border border-blue-700 h-[46px] w-[270px] bg-white rounded-s-lg"
+      className="flex items-center border border-colorText h-auto w-[270px] bg-white rounded-s-lg"
       style={{
         borderRadius: "10px",
       }}
     >
-      <div className="mr-2 ml-2 h-[14px] w-[14px]">
-        <input type="checkbox" className="form-checkbox text-green-500" />
+      <div className="mr-2 ml-2 h-[14px] w-[14px] flex items-center border-colorText">
+        <input type="checkbox" className="form-checkbox border-colorText" />
       </div>
+      <div className="grid grid-cols-2 flex items-center border-colorText">
+        <div className="mr-2 text-blue-700 font-Poppins text-base text-xs font-normal leading-4 text-colorText">
+          Amenabar 6463 dsada dsa d as d as das da, CABA
+        </div>
 
-      <div className="mr-2 text-blue-700 w-[72px] h-[30px] font-Poppins text-base font-normal leading-4">
-        Amenabar 2353, <br />
-        CABA
-      </div>
-
-      <div
-        className="flex justify-center items-center border border-blue-700 h-[26px] w-[59px]"
-        style={{
-          borderRadius: "10px",
-        }}
-      >
-        <button onClick={handleSubtract}>
-          <img
-            className="h-3 w-3 mr-1"
-            src="/minusCircle.svg"
-            alt="minusCircle"
-          />
-        </button>
-        <p className="text-blue-700">{amount}</p>
-        <button onClick={handleAdd}>
-          <img
-            className="h-3 w-3 ml-1"
-            src="/plus-circle.svg"
-            alt="plus-circle"
-          />
-        </button>
+        <div
+          className="flex justify-center items-center border border-colorText h-[26px] w-[59px] ml-12"
+          style={{
+            borderRadius: "10px",
+          }}
+        >
+          <button onClick={handleSubtract}>
+            <img
+              className="h-3 w-3 mr-1"
+              src="/minusCircle.svg"
+              alt="minusCircle"
+            />
+          </button>
+          <p className="text-colorText">{amount}</p>
+          <button onClick={handleAdd}>
+            <img
+              className="h-3 w-3 ml-1"
+              src="/plus-circle.svg"
+              alt="plus-circle"
+            />
+          </button>
+        </div>
       </div>
     </div>
   );
